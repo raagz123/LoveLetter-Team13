588c3b31bcd96a07386057abaf9152a6dbeb476b
mc paul jr <msoriano@deakin.edu.au>
2020-10-28 00:47:19 +1100

    fixed folders

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+//GET homepage.
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+//GET rules
+router.get('/rules', function(req, res) {
+    res.render('rules');
+});
+
+//GET about
+router.get('/about', function(req, res) {
+    res.render('about');
+});
+
+//GET private
+router.get('/private', function(req, res) {
+    res.render('private');
+});
+
+//GET GameCreation
+router.get('/GameCreation', function(req, res) {
+    res.render('GameCreation');
+});
+
+
+  
+module.exports=router;
+
